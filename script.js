f92d68b0ef543720b0cdb3fdcc6c6c0982bf398c
Mukhtar <mn5feterless@gmail.com>
2025-08-11 19:38:42 +0500

    add the working timer element

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ const quote = "The quick brown fox jumps over the lazy dog.";
 const quoteElement = document.getElementById("quote");
 const inputElement = document.getElementById("input");
 const resultElement = document.getElementById("result");
+const timerElement = document.getElementById("timer");
 
 let startTime = null;
 let ended = false;
+
 
 inputElement.addEventListener("input", ()=> {
     const input = inputElement.value;
 
     if(!startTime && input.length > 0) {
         startTime = new Date();
+        startTimer();
+        
+    }
+
+    function startTimer() {
+        timerID = setInterval(()=>{
+            const timeStamp = new Date();
+            const timePassed = Math.floor((timeStamp-startTime) / 1000);
+            const secunde = timePassed % 60;
+            const minute = Math.floor(timePassed / 60);
+
+            timerElement.textContent = `${minute.toString().padStart(2,'0')}:${secunde.toString().padStart(2,'0')}`
+        },1000);
+    }
+
+    function stopTimer() {
+        clearInterval(timerID);
     }
 
     if(input === quote) {
@@ -25,6 +44,8 @@ inputElement.addEventListener("input", ()=> {
             resultElement.textContent = `wpm: ${wpm}`;
 
             inputElement.disabled = true;
+            stopTimer();    
+
         }
     }
 });
